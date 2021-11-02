352e250b45d6b2fdcff0b3b2d3d192573eab9778
shane <shane@netlify.com>
2021-11-02 16:55:59 -0700

    ts logs

--- netlify/functions/hello-world-ts/hello-world-ts.ts
+++ netlify/functions/hello-world-ts/hello-world-ts.ts
@@ -1,7 +1,19 @@
 import { Handler } from '@netlify/functions'
+import { Logger } from "tslog";
 
+const log: Logger = new Logger({ name: "myLogger" });
 export const handler: Handler = async (event, context) => {
   const { name = 'stranger' } = event.queryStringParameters
+
+  console.log("type-script console log")
+  console.error("type-script console error log")
+log.silly("I am a silly log.");
+log.trace("I am a trace log with a stack trace.");
+log.debug("I am a debug log.");
+log.info("I am an info log.");
+log.warn("I am a warn log with a json object:", { foo: "bar" });
+log.error("I am an error log.");
+log.fatal(new Error("I am a pretty Error with a stacktrace."));
 
   return {
     statusCode: 200,
