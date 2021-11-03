96b89b77d2e30dfcb79003da11b970a05b21569a
shane <shane@netlify.com>
2021-11-02 17:09:56 -0700

    npm log levles

--- netlify/functions/hello-world-js/hello-world-js.js
+++ netlify/functions/hello-world-js/hello-world-js.js
@@ -1,4 +1,7 @@
 const winston = require('winston');
+var npmlog = require('npmlog')
+var loglevel = require('loglevel');
+
 const logger = winston.createLogger({
   format: winston.format.simple(),
   transports: new winston.transports.Console()
@@ -6,12 +9,23 @@ const logger = winston.createLogger({
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
   try {
-    logger.silly("I am a silly log.");
-    logger.debug("I am a debug log.");
-    logger.info("I am an info log.");
-    logger.warn("I am a warn log with a json object:", { foo: "bar" });
-    logger.error("I am an error log.");
+    
+    logger.debug("I am a debug winstonlog.");
+    logger.info("I am an info winstonlog.");
+    logger.warn("I am a warn winstonlog with a json object:", { foo: "bar" });
+    logger.error("I am an error winstonlog.");
 
+    
+    // npmlog.debug("I am a debug log.");
+    npmlog.info("I am an info npmlog.");
+    npmlog.warn("I am a warn npmlog with a json object:", { foo: "bar" });
+    npmlog.error("I am an error npmlog.");
+
+    loglevel.enableAll()
+    loglevel.debug("I am a debug loglevel.");
+    loglevel.info("I am an info loglevel.");
+    loglevel.warn("I am a warn loglevel with a json object:", { foo: "bar" });
+    loglevel.error("I am an error loglevel.");
     const subject = event.queryStringParameters.name || 'World'
     console.log("console log line")
     console.error("console error log line")
