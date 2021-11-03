da2cdf1d9ffef51d89e5b3d9ce6f399407d40f36
shane <shane@netlify.com>
2021-11-03 10:16:21 -0700

    change log

--- netlify/functions/hello-world-ts/hello-world-ts.ts
+++ netlify/functions/hello-world-ts/hello-world-ts.ts
@@ -8,7 +8,6 @@ export const handler: Handler = async (event, context) => {
   console.log("type-script console log")
   console.error("type-script console error log")
   log.silly("I am a silly tslog.");
-  log.trace("I am a trace tslog with a stack trace.");
   log.debug("I am a debug tslog.");
   log.info("I am an info tslog.");
   log.warn("I am a warn tslog with a json object:", { foo: "bar" });
