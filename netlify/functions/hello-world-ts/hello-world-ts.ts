e1181e65fb02d721428744f42c6a74755aba777b
Shane Smith-Sahnow <ShaneSaww@users.noreply.github.com>
2021-11-15 12:47:37 -0800

    Update hello-world-ts.ts

--- netlify/functions/hello-world-ts/hello-world-ts.ts
+++ netlify/functions/hello-world-ts/hello-world-ts.ts
@@ -5,7 +5,7 @@ const log: Logger = new Logger({ name: "myLogger" });
 export const handler: Handler = async (event, context) => {
   const { name = 'stranger' } = event.queryStringParameters
 
-  console.log("type-script console log")
+  console.log("type-script console l0g")
   console.error("type-script console error log")
   console.log("type-script console log after error")
   log.silly("I am a silly tslog.");
